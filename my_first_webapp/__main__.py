8a037c2f491a81468d740ce9a07438f5a8b46db7
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2017-08-11 10:26:26 +0100

    Try global again?

--- my_first_webapp/__main__.py
+++ my_first_webapp/__main__.py
@@ -5,17 +5,19 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
+global_get_count = 0
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         time_str = datetime.datetime.now().isoformat()
         msg = 'Hello, world ({}) : #{}'
-        is_original = not hasattr(self, 'get_count')
-        if is_original:
-            self.get_count = 0
-            msg += '  (first)'
-        self.get_count += 1
-        self.write(msg.format(time_str, self.get_count))
+#        is_original = not hasattr(self, 'get_count')
+#        if is_original:
+#            self.get_count = 0
+#            msg += '  (first)'
+#        self.get_count += 1
+        global_get_count += 1
+        self.write(msg.format(time_str, global_get_count))
 
 
 def main():
