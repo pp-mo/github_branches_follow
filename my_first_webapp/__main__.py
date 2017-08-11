212f0c67fffdf6d35024d219a1e6dac9e7b56a9b
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2017-08-11 10:52:19 +0100

    Add post.

--- my_first_webapp/__main__.py
+++ my_first_webapp/__main__.py
@@ -1,6 +1,8 @@
 import datetime
 import os
 
+import json
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
@@ -10,6 +12,19 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         time_str = datetime.datetime.now().isoformat()
         msg = 'Hello, world! ...  TIME = {}'
+        self.write(msg.format(time_str))
+
+    def post(self):
+        received_json = self.get_body_argument("message")
+        received_objects = json.loads(received_json)
+        len_json = len(received_json)
+        rx_type = type(received_objects)
+#        len_objs = len(received_objects)
+#        if rx_type == dict:
+#            rx_names = received_objects.keys()
+        self.set_header("Content-Type", "text/plain")
+        msg = 'received: strlen={} rxtype={}'.format(
+            len_json, rx_type)
         self.write(msg.format(time_str))
 
 
